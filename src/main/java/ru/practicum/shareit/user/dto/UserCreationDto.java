bc32d677d2271d27a70df2c2e1347f5e319a8ceb
ChertkovFedor <ChertkovFedor@yandex.ru>
2023-03-30 14:37:11 +0530

    Добавил валидацию в DTO Item и User

--- src/main/java/ru/practicum/shareit/user/dto/UserCreationDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserCreationDto.java
@@ -4,11 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 @Data
 @AllArgsConstructor
 @NonNull
